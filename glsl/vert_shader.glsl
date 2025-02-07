41f2588d653c4cd45a3d5fc511ba702b4b611cb6
Lucy Faria <rafploooz@gmail.com>
2025-02-07 13:23:52 -0500

    Hello, triangle

--- glsl/vert_shader.glsl
+++ glsl/vert_shader.glsl
@@ -1,3 +1,9 @@
 R"(
+#version 330 core
+layout (location = 0) in vec3 attribute_Position;
 
+void main()
+{
+    gl_Position = vec4(attribute_Position.x, attribute_Position.y, attribute_Position.z, 1.0);
+}
 )"
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "../include/glad/glad.h"
+#include <GL/gl.h>
 #include <GLFW/glfw3.h>
-#include <string>
 
 #include "../include/include.hpp"
 
@@ -24,6 +24,7 @@ public:
 
 private:
   GlfwWindowWrapper() = default;
+  ~GlfwWindowWrapper() { glfwDestroyWindow(this->window); }
   static std::optional<GlfwWindowWrapper *> create() {
     std::cout << "Making ::create()\n";
     glfwInit();
@@ -36,7 +37,6 @@ private:
 
     if (window == NULL) {
       std::cout << "Failed to create GLFW window\n";
-      glfwTerminate();
       return std::nullopt;
     }
     glfwMakeContextCurrent(window);
@@ -65,39 +65,109 @@ public:
       return instance;
     }
   };
-  static auto clear() {
+  static auto clear_all_windows() {
     if (instance != NULL) {
       delete instance;
     }
     instance = NULL;
+    glfwTerminate();
   }
 };
+
+const char *fragment_shader_text =
+#include "../glsl/frag_shader.glsl"
+    ;
+
+const char *vertex_shader_text =
+#include "../glsl/vert_shader.glsl"
+    ;
 
 // clang-format off
 float vertices[] = {
   -0.5f,-0.5f,0.0f,
-  0.5f, -0.5f,0.0f,
+  0.5f, -0.2f,0.0f,
   0.0f, 0.5f, 0.0f
 };
-
-const char* fragment_shader_text = 
-#include "../glsl/frag_shader.glsl"
-;
-
-const char* vertex_shader_text =
-#include "../glsl/vert_shader.glsl"
-;
-
 // clang-format on
-void render() {
 
-  GLuint VBO;
+struct RenderInfo {
+  GLuint vao;
+  GLuint vbo;
+  GLuint program;
+};
+
+const RenderInfo set_up_render() {
+  GLuint VBO = 0;
   glGenBuffers(1, &VBO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
+  GLuint VAO;
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+
+  // Copy vertex data into the bound array buffer VBO
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // Create the vertex shader
+  GLuint vert_shader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vert_shader, 1, &vertex_shader_text, NULL);
+  glCompileShader(vert_shader);
+
+  int success;
+  glGetShaderiv(vert_shader, GL_COMPILE_STATUS, &success);
+
+  if (success != GL_TRUE) {
+    char infoLog[512];
+    glGetShaderInfoLog(vert_shader, 512, NULL, infoLog);
+    std::cout << "ERROR COMPILING VERTEX SHADER: " << infoLog;
+    exit(2);
+  }
+
+  GLuint frag_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(frag_shader, 1, &fragment_shader_text, NULL);
+  glCompileShader(frag_shader);
+
+  glGetShaderiv(frag_shader, GL_COMPILE_STATUS, &success);
+  if (success != GL_TRUE) {
+    char infoLog[512];
+    glGetShaderInfoLog(frag_shader, 512, NULL, infoLog);
+    std::cout << "ERROR COMPILING FRAGMENT SHADER: " << infoLog;
+    exit(3);
+  }
+
+  let program = glCreateProgram();
+
+  glAttachShader(program, vert_shader);
+  glAttachShader(program, frag_shader);
+  glLinkProgram(program);
+
+  glGetProgramiv(program, GL_LINK_STATUS, &success);
+  if (success != GL_TRUE) {
+    char infoLog[512];
+    glGetShaderInfoLog(frag_shader, 512, NULL, infoLog);
+    std::cout << "ERROR LINKING PROGRAM: " << infoLog;
+    exit(4);
+  }
+
+  glDeleteShader(vert_shader);
+  glDeleteShader(frag_shader);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, NULL);
+  glEnableVertexAttribArray(0);
+
+  return RenderInfo{.vao = VAO, .vbo = VBO, .program = program};
+}
+
+void clear() {
   glClearColor(0.2f, 0.2f, 0.3f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT);
+}
+void render(GLuint program, GLuint vao) {
+  clear();
+
+  glUseProgram(program);
+  glBindVertexArray(vao);
+  glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 int main(void) {
@@ -109,12 +179,15 @@ int main(void) {
 
   glViewport(0, 0, 800, 600);
 
+  let info = set_up_render();
+
   mut window = window_wrapper->window;
   while (!glfwWindowShouldClose(window)) {
-    render();
+    render(info.program, info.vao);
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
+  GlfwWindowWrapper::clear_all_windows();
   return 0;
 }
